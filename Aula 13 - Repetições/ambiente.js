26cc69a11368edeaca5393a9eb3563961cb8236a
alvesgit-hub <131784167+alvesgit-hub@users.noreply.github.com>
2023-07-06 20:12:21 -0300

    atualizado

---
+++ Aula 13 - Repetições/ambiente.js
@@ -0,0 +1,29 @@
+/*console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+*/
+
+/*
+var c = 1
+while(c<=6){
+    console.log(`Passo ${c}`)
+    c++ incremento// c = c+1 
+}
+*/
+
+
+
+/*
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+} while (c<=6)*/
+
